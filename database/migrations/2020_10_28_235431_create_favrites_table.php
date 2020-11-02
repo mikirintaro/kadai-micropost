da93bfe21524536e6898abe2ced2b56ee4e9fd49
mikirintaro <k.s.gandhi@nifty.com>
2020-11-02 12:04:42 +0000

    favorite

--- database/migrations/2020_10_28_235431_create_favrites_table.php
+++ database/migrations/2020_10_28_235431_create_favrites_table.php
@@ -21,7 +21,7 @@ class CreateFavritesTable extends Migration
             
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('micropost_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('micropost_id')->references('id')->on('microposts')->onDelete('cascade');
             
             $table->unique(['user_id', 'micropost_id']);
         });
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -21,19 +21,19 @@
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
                         @endif
-                        @if (Auth::id() != $micropost->user_id)
-                            @if (Auth::user()->is_favorite($micropost->id))
-                                {{-- お気に入り削除ボタンのフォーム --}}
-                                {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-                                    {!! Form::submit('Unfavorite', ['class' => "btn btn-warning btn-sm"]) !!}
-                                {!! Form::close() !!}
-                            @else
-                                {{-- お気に入り登録ボタンのフォーム --}}
-                                {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-                                    {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
-                                {!! Form::close() !!}
-                            @endif
+                       
+                        @if (Auth::user()->is_favorite($micropost->id))
+                            {{-- お気に入り削除ボタンのフォーム --}}
+                            {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('Unfavorite', ['class' => "btn btn-warning btn-sm"]) !!}
+                            {!! Form::close() !!}
+                        @else
+                            {{-- お気に入り登録ボタンのフォーム --}}
+                            {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
+                                {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
+                            {!! Form::close() !!}
                         @endif
+                      
                     </div>
                 </div>
             </li>
